6543799d930643f8060f4c2cc663ad772fab9558
Arthur Novaes <arthurnovaes55@gmail.com>
2024-11-19 10:52:44 -0300

    feat: busca dados inicialmente

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,17 @@
       ></div>
 
       <br />
-      <button type="submit" id="avisos">Avisos</button>
-      <button type="submit" id="impugnacoes">Impugnações</button>
-      <button type="submit" id="esclarecimentos">Esclarecimentos</button>
+      <button type="submit" id="avisos" ref="avisos">Avisos</button>
+      <button type="submit" id="impugnacoes" ref="impugnacoes">
+        Impugnações
+      </button>
+      <button type="submit" id="esclarecimentos" ref="esclarecimentos">
+        Esclarecimentos
+      </button>
     </form>
 
     <div id="response">
-      <pre>{{ response }}</pre>
+      <pre>{{ responses }}</pre>
     </div>
   </div>
 </template>
@@ -28,35 +32,41 @@ export default {
     return {
       siteKey: "b8bbded1-9d04-4ace-9952-b67cde081a7b",
       captchaId: null, // Armazena o ID do hCaptcha
-      response: "",
+      responses: [],
       activeButtonId: null,
     };
   },
-  mounted() {
+  async mounted() {
     // Carrega o script do hCaptcha
     const script = document.createElement("script");
     script.src = "https://hcaptcha.com/1/api.js";
     script.async = true;
     script.defer = true;
-    script.onload = this.initializeCaptcha; // Inicializa o hCaptcha após carregar
+    script.onload = this.initializeCaptcha;
     document.head.appendChild(script);
 
-    // Define a callback global para o hCaptcha
     window.onSubmit = this.onSubmit;
+    const btnAvisos = this.$refs.avisos;
+    const btnImpugnacoes = this.$refs.impugnacoes;
+    const btnEsclarecimentos = this.$refs.esclarecimentos;
+    setTimeout(() => {
+      btnAvisos.click();
+      btnImpugnacoes.click();
+      btnEsclarecimentos.click();
+    }, 2000);
   },
   methods: {
-    initializeCaptcha() {
-      // Renderiza manualmente o hCaptcha
-      this.captchaId = window.hcaptcha.render(this.$refs.captcha, {
+    async initializeCaptcha() {
+      this.captchaId = await window.hcaptcha.render(this.$refs.captcha, {
         sitekey: this.siteKey,
         size: "invisible",
         callback: this.onSubmit,
       });
     },
     submitForm(event) {
-      const button = event.submitter; // Obtém o botão que disparou o evento
+      const button = event.submitter;
       this.activeButtonId = button.id;
-      window.hcaptcha.execute(this.captchaId); // Executa o hCaptcha manualmente
+      window.hcaptcha.execute(this.captchaId);
     },
     onSubmit(token) {
       const url = new URL(
@@ -67,10 +77,10 @@ export default {
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
-          this.response = JSON.stringify(data, null, 2);
+          this.responses = [...this.responses, data];
         })
         .catch((error) => {
-          this.response = `Erro: ${error}`;
+          this.responses = `Erro: ${error}`;
         });
     },
   },
